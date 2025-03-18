34c6b58e52ae1c9409100072202c7c718ab5b09e
João Henrique <souza.joaohenrique.rocha@gmail.com>
2025-03-17 22:01:40 -0300

    config Manseira

---
+++ banco.js
@@ -0,0 +1,62 @@
+const mysql = require('mysql2/promise'); 
+
+async function conectarBD()
+{ 
+    if(global.conexao && global.conexao.state !== 'disconnected') 
+    {
+        return global.conexao;
+    }
+    
+    /*
+    const connectionString = 'mysql://root:senha@localhost:3306/livraria' 
+    const connection= await mysql.createConnection(connectionString)
+    */
+    
+    const conexao = await mysql.createConnection(
+        { 
+            host     : 'localhost', 
+            port     : 3306, 
+            user     : 'root',
+            password : '', 
+            database : 'flix' 
+        }); 
+        
+    console.log('Conectou no MySQL!'); 
+
+    global.conexao = conexao; 
+    return global.conexao; 
+} 
+
+
+async function buscarUsuario(usuario)
+{
+    const conexao = await conectarBD();
+    const sql = "select * from user where email=? and senha=?;";
+    const [usuarioEcontrado] = await conexao.query(sql,[usuario.email, usuario.senha]);
+    return usuarioEcontrado && usuarioEcontrado.length>0 ? usuarioEcontrado[0] : {};
+}
+
+
+async function buscarPerfis(usuario)
+{
+    const conexao = await conectarBD();
+    const sql = "select * from perfil where codperfil=?;";
+    const [perfisEcontrados] = await conexao.query(sql,[usuario]);
+    return perfisEcontrados;
+}
+
+
+async function buscarPerfil(codigoPerfil)
+{
+    const conexao = await conectarBD();
+    const sql = "select * from perfil where codperfil=?;";
+    const [dadosEcontrados] = await conexao.query(sql,[codigoPerfil]);
+    return dadosEcontrados[0];
+}
+
+
+
+
+conectarBD()
+
+module.exports = { buscarUsuario, buscarPerfis, buscarPerfil }
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,93 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+/**
+ * 
+ * Rotas GET de usuários
+ * 
+ */
+
+
+/* GET home page (login) */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'flix - Entrar' });
+  if (global.usuarioEmail && global.usuarioEmail != "") {
+    res.redirect('/browse');
+  }
+
+  res.render('index', { titulo: 'MFlix - Login' });
 });
 
-router.post('/login', function(req, res, next){
-  // captura os dados do forms
-  const email = req.body.email
-  const senha = req.body.senha
 
-  //verifica no bd
 
-  
+/* GET escolha de perfil */
+router.get('/perfis', async function(req, res, next) {
+  verificarLogin(res);
+  const registrosPerfis = await global.banco.buscarPerfis(global.cod);
 
+  if (registrosPerfis.length == 1)
+  {
+    global.codperfil = registrosPerfis[0].codperfil
+    res.redirect('/browse');
+  } 
+
+  res.render('perfis', { titulo: 'MFlix - Escolha de Perfil', registrosPerfis });
+});
+
+
+
+/* GET registrar perfil escolhido */
+router.get('/entrarPerfil/:id', async function(req, res, next) {
+  verificarLogin(res);
+  const codigoPerfil = parseInt(req.params.id);
+  const dadosPerfil = await global.banco.buscarPerfil(codigoPerfil);
+  global.perfil = dadosPerfil;
+  res.redirect('/browse');
+});
+
+
+
+/* GET escolha de vídeo */
+router.get('/browse', function(req, res, next) {
+  verificarLogin(res);
+  res.render('browse', { titulo: 'MFlix - Escolha de Vídeo', imagem: global.perfil.foto });
+});
+
+
+
+/**
+ * 
+ * Rotas POST de usuários
+ * 
+ */
+
+
+/* POST login */
+router.post('/login', async function(req, res, next){
+  const email = req.body.email ;
+  const senha = req.body.senha;
+
+  const usuario = await global.banco.buscarUsuario({email,senha});
+
+  global.usuarioCodigo = usuario.cod;
+  global.usuarioEmail = usuario.email;
+  res.redirect('/perfis');
 })
+
+
+
+/**
+ * 
+ * Funções diversas
+ * 
+ */
+
+function verificarLogin(res)
+{
+  if (!global.usuarioEmail || global.usuarioEmail == "")
+    res.redirect('/');
+}
+
+
+
 module.exports = router;
